dc4b78ea5495f57b0fcddfba2e69785e27ebbe92
PreslavKisyov <preslavkisyov98@gmail.com>
2019-02-19 23:01:58 +0000

    Add 'prep' project with Vue.js

---
+++ prep/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCT9bHYhOxHhYC20EB0oouBXpiVV9O-D_U",
+    authDomain: "prep-playground-f1574.firebaseapp.com",
+    databaseURL: "https://prep-playground-f1574.firebaseio.com",
+    projectId: "prep-playground-f1574",
+    storageBucket: "prep-playground-f1574.appspot.com",
+    messagingSenderId: "690385598299"
+}
