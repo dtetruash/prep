cba0ce4f17fdc421ec5a17c9f103424df22fb9bd
ARZHAN\arzhtong <arzhtong@gmail.com>
2019-02-22 02:24:28 +0000

    Begin adding images on apppointment prep

--- prep/lib/appointment_prep.dart
+++ prep/lib/appointment_prep.dart
@@ -8,7 +8,7 @@ class AppointmentPrep extends StatelessWidget {
     return new Scaffold(
       body: new ListView(
         children: <Widget>[
-            CategoryCard("Allowed foods", 139, Colors.green[200], Colors.yellow[200],'assets/images/vegpacket.png'),
+            CategoryCard("Allowed foods", 139, Colors.green[200], Colors.yellow[200],'assets/vegpacket.png'),
             CategoryCard("Not allowed foods", 50, Colors.red[200], Colors.orange[200],'assets/images/friedchicken.jpeg'),
             CategoryCard("Extra information", 63, Colors.orange[100], Colors.pink[100],'assets/images/friedchicken.jpeg'),
             CategoryCard("Other category", 73, Colors.blue[200], Colors.green[200],'assets/images/friedchicken.jpeg'),
@@ -40,7 +40,7 @@ class CategoryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) { 
     var assetimage=AssetImage(location);
-    var image=new Image(image: assetimage,height: 48.0, width: 24.0, fit: BoxFit.fitWidth);
+    var image=new Image(image: assetimage,height: 24.0, width: 24.0, fit: BoxFit.fitWidth);
     return Card(
       elevation: 3.0,
       child: InkWell(
@@ -54,26 +54,26 @@ class CategoryCard extends StatelessWidget {
           children: <Widget>[
         new Expanded(
         child: new Column(
-          mainAxisSize: MainAxisSize.min,
+          // mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-             Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomLeft,
-                  stops: [0.1, 0.9],
-                  colors: [
-                    left,
-                    right,
-                  ],
-                ),
-              ),
-              height: 90.0,
-            ),
+            //  Container(
+            //   width: double.infinity,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            //     gradient: LinearGradient(
+            //       begin: Alignment.topLeft,
+            //       end: Alignment.bottomLeft,
+            //       stops: [0.1, 0.9],
+            //       colors: [
+            //         left,
+            //         right,
+            //       ],
+            //     ),
+            //   ),
+            //   height: 90.0,
+            // ),
             ListTile(
-              leading: Image.asset(location),
+              leading: new ImageIcon(new AssetImage('assets/images/vegpacket.png'), size: 80.0, color: Colors.blue),
               title: Text(categoryName),
               subtitle: Text(numberOfItems.toString() + " items"),
               
