45d0f6965afb9a29d07166f0bef1763d92e0d4cb
David Simon Tetruashvili <davidsimon.t@gmail.com>
2019-02-22 12:18:33 +0000

    :art: Encapsulate Text composer; Update gitignore
    This commit is not functional. The code was updates to follow Dart 2 conventions.

--- prep/lib/screens/messaging.screen.dart
+++ prep/lib/screens/messaging.screen.dart
@@ -1,63 +1,75 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/query.dart';
 
 class Messaging extends StatefulWidget {
   @override
-  State createState() => new MessagingScreen();
+  State createState() => MessagingScreen();
 }
 
 class MessagingScreen extends State<Messaging> with TickerProviderStateMixin {
-  final MessagingQueries _queries = new MessagingQueries();
-  final TextEditingController _textController = new TextEditingController();
+  final TextEditingController _textController = TextEditingController();
   bool _hasTyped = false;
+  List<_Message> _messagesList;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Column(children: <Widget>[
-        new Flexible(
-          child: _messagesStream(MessagingQueries().messagesSnapshots),
+    return Scaffold(
+      body: Column(children: <Widget>[
+        Flexible(
+          child: _messagesStream(MessagingQueries().messagesSnapshots)
         ),
-        new Divider(height: 1.0),
-        new Container(
-          child: Row(children: <Widget>[
-            new Flexible(
-              child: new TextField(
-                controller: _textController,
-                onChanged: (string) {
-                  setState(() {
-                    //TODO (unimportant) create a better metric
-                    _hasTyped = string.length > 0;
-                  });
-                },
-              ),
-            ),
-            new Container(
-              child: new IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  _hasTyped ? _queries.sendMessage(_textController.text) : null;
-                  setState(() {
-                    _textController.clear();
-                    _hasTyped = false;
-                  });
-                },
-              ),
-            )
-          ]),
-        ),
+        Divider(height: 1.0),
+        Container(
+          child: _biuldTextComposer(),
+          decoration: BoxDecoration(color: Theme.of(context).cardColor),
+        )
       ]),
     );
   }
 
-    StreamBuilder<QuerySnapshot> _messagesStream(Stream<QuerySnapshot> snapshots) {
+  Widget _biuldTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onChanged: (userInputText) {
+                  setState(() {
+                    _hasTyped = userInputText.length > 0;
+                  });
+                },
+                onSubmitted: MessagingQueries().sendMessage,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Type a message"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSendIconTap(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  StreamBuilder<QuerySnapshot> _messagesStream(
+      Stream<QuerySnapshot> snapshots) {
     return StreamBuilder<QuerySnapshot>(
         stream: snapshots,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
             //TODO (for production) print to console log.
-            return new Text('Error: ${snapshot.error}');
+            return Text('Error: ${snapshot.error}');
           }
 
           switch (snapshot.connectionState) {
@@ -68,9 +80,9 @@ class MessagingScreen extends State<Messaging> with TickerProviderStateMixin {
             default:
               List<Widget> children =
                   snapshot.data.documents.map((DocumentSnapshot document) {
-                Map<String, dynamic> map = document.data;
-                return new _Message(
-                    map['message'], map['datetime'], map['isPatient']);
+                Map<String, dynamic> message = document.data;
+                return _Message(message['content'], message['datetime'],
+                    message['isPatient']);
               }).toList();
               return _messageListView(children);
           }
@@ -79,13 +91,18 @@ class MessagingScreen extends State<Messaging> with TickerProviderStateMixin {
 
   ListView _messageListView(List<Widget> childrenIn) {
     bool isReverse = true;
-    EdgeInsets insets = new EdgeInsets.all(6.0);
+    EdgeInsets insets = EdgeInsets.all(6.0);
 
     return (childrenIn == null)
-        ? new ListView(reverse: isReverse, padding: insets)
-        : new ListView(
-            reverse: isReverse, padding: insets, children: childrenIn);
+        ? ListView(reverse: isReverse, padding: insets)
+        : ListView(reverse: isReverse, padding: insets, children: childrenIn);
   }
+
+  void _sendMessage(String messageText) =>
+      MessagingQueries().sendMessage(messageText);
+
+  void _handleSendIconTap(String messageText) =>
+      _hasTyped ? MessagingQueries().sendMessage(messageText) : null;
 }
 
 class _Message extends StatelessWidget {
@@ -97,15 +114,13 @@ class _Message extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Alignment alignmentMessage = (isPatient) ? Alignment.centerRight : Alignment.centerLeft;
+    Alignment alignmentMessage =
+        (isPatient) ? Alignment.centerRight : Alignment.centerLeft;
 
-    return new Container(
-      margin: const EdgeInsets.only(top: 6.0),
-      child: new Text(messageText),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Text(messageText),
       alignment: alignmentMessage,
     );
   }
 }
-
-
-
