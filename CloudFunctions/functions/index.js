3e5887c08eaca577d8a6da6de3774fcefbb272ea
fsarunas <fsarunas@protonmail.ch>
2019-02-24 23:48:05 +0000

    clean up code

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -11,12 +11,13 @@ exports.deleteUser = functions
     .document('users/{userID}')
     .onDelete((snapshot, context) => {
         const email = snapshot.data().email;
-    
+
         return admin.auth().getUserByEmail(email)
-        .then((userRecord) => {
-            const userUID = userRecord.toJSON().uid;
-            return admin.auth().deleteUser(userUID);
-        });
+            .then((userRecord) => {
+                const userUID = userRecord.toJSON().uid;
+
+                return admin.auth().deleteUser(userUID);
+            });
     });
 
 // Updates a user's email address in auth
@@ -24,20 +25,17 @@ exports.updateUserEmail = functions
     .region('europe-west1')
     .firestore
     .document('users/{userID}')
-    .onUpdate((change, contxt) => {
+    .onUpdate((change, context) => {
         const emailBefore = change.before.data().email;
         const emailAfter = change.after.data().email;
 
-        if(emailBefore !== emailAfter) {
-            return admin.auth().getUserByEmail(emailBefore)
-            .then((userRecord) => {
-                const userUID = userRecord.toJSON().uid;
-                return admin.auth().updateUser(userUID, {
-                    email: emailAfter
+        return (emailBefore === emailAfter) ? null
+            : admin.auth().getUserByEmail(emailBefore)
+                .then((userRecord) => {
+                    const userUID = userRecord.toJSON().uid;
+
+                    return admin.auth().updateUser(userUID, {email: emailAfter});
                 });
-            });
-        }
-        return null;
     });
 
 // Sends a notification to a topic with the name {appointmentID}
@@ -46,22 +44,19 @@ exports.sendNotification = functions
     .firestore
     .document('appointments/{appointmentID}/messages/{messageID}')
     .onCreate((snapshot, context) => {
-        if (!snapshot.data().isPatient) {
-            const appointmentID = context.params.appointmentID;
-            //const messageID = context.params.messageID;
+        const appointmentID = context.params.appointmentID;
+        //const messageID = context.params.messageID;
+        const notificationContent = {
+            notification: {
+                title: "/*App name */",
+                body: "You have a new message!",
+                icon: "default",
+                click_action: "/*Package */_TARGET_NOTIFICATION"
+            }
+        };
 
-            const notificationContent = {
-                notification: {
-                    title: "/*App name */",
-                    body: "You have a new message!",
-                    icon: "default",
-                    click_action: "/*Package */_TARGET_NOTIFICATION"
-                }
-            };
-
-            return admin
-                .messaging()
-                .sendToTopic('/topics/' + appointmentID, notificationContent);
-        }
-        return null;
+        return (snapshot.data().isPatient) ? null
+            : admin
+            .messaging()
+            .sendToTopic('/topics/' + appointmentID, notificationContent);
     });
