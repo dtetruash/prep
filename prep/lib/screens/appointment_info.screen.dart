bd782e5bf7087ce3ef59623f1f0f5b600512a124
codedario <darionunez1999@gmail.com>
2019-03-01 12:45:51 +0000

    Remove unnecessary import statements

--- prep/lib/screens/appointment_info.screen.dart
+++ prep/lib/screens/appointment_info.screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import './appointment.screen.dart';
 
 class AppointmentInfo extends StatefulWidget {
   @override
--- prep/lib/screens/daily_checkups.screen.dart
+++ prep/lib/screens/daily_checkups.screen.dart
@@ -1,13 +1,4 @@
-import 'dart:io';
-import 'dart:async';
-import 'package:path_provider/path_provider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/services.dart';
-
-import './appointment.screen.dart';
 
 class DailyCheckups extends StatefulWidget {
   @override
