ad323d6a67038c419e09ea5b859744a97ecd4753
Arzhan Tong <arzhtong@gmail.com>
2019-03-06 14:38:01 +0000

    Remove branch irrelavant files

--- prep/lib/screens/daily_checkups.screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DailyCheckups extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text("daily cehckups");
-  }
-}
