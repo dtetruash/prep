9678fdf49086ce9bb597f723e718ae9f48a275e1
codedario <darionunez1999@gmail.com>
2019-03-15 22:36:59 +0000

    Refactor the appintment prep screen class

--- prep/lib/screens/appointment_prep.screen.dart
+++ prep/lib/screens/appointment_prep.screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:prep/screens/recipeList.screen.dart';
-import 'information_parser.screen.dart';
-import 'faq_parser.screen.dart';
-import 'package:prep/screens/category.screen.dart';
-import 'package:prep/utils/query.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'package:prep/utils/query.dart';
+import 'package:prep/widgets/appointment_prep/category_card.dart';
 
 class AppointmentPrep extends StatefulWidget {
   final DateTime _appointmentDatetime;
@@ -42,116 +40,6 @@ class _AppointmentPrepState extends State<AppointmentPrep> {
       document['type'],
       Colors.white,
       widget._appointmentDatetime
-    );
-  }
-}
-
-class CategoryCard extends StatelessWidget {
-  final String contents;
-  final String title;
-  final String type;
-  final Color color;
-  final DateTime _appointmentDateTime;
-
-  CategoryCard(this.contents, this.title, this.type, this.color, this._appointmentDateTime);
-
-  Future _navigate(dynamic context) {
-    //TODO: add links to the relevant pages
-
-    switch (type) {
-      case "informations":
-        return Navigator.push(context, MaterialPageRoute(builder: (context) => InformationParser(contents)));
-      case "categoryList":
-        return Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryListParser(contents)));
-      case "recipeView":
-        return Navigator.push(context,
-            MaterialPageRoute(builder: (context) => RecipeListScreen()));
-      default:
-        return Navigator.push(context, MaterialPageRoute(builder: (context) => FaqParser(_appointmentDateTime)));
-    }
-  }
-
-  CircleAvatar _getIcon() {
-    if (type == "categoryList") {
-      return CircleAvatar(
-        backgroundColor: Colors.blue[400],
-        child: Icon(
-          Icons.list,
-          color: Colors.white,
-        ),
-      );
-    } else if (type == "informations") {
-      return CircleAvatar(
-        backgroundColor: Colors.deepPurple[400],
-        child: Icon(
-          Icons.info,
-          color: Colors.white,
-        ),
-      );
-    } else if (type == "faqs") {
-      return CircleAvatar(
-        backgroundColor: Colors.green[400],
-        child: Icon(
-          Icons.live_help,
-          color: Colors.white,
-        ),
-      );
-    } else {
-      return CircleAvatar(
-        backgroundColor: Colors.red[400],
-        child: Icon(
-          Icons.local_dining,
-          color: Colors.white,
-        ),
-      );
-    }
-  }
-
-  String _getCategory(){
-    switch (type) {
-      case "informations":
-        return "Info";
-      case "categoryList":
-        return "List";
-      case "recipeView":
-        return "Recipe";
-      default:
-        return "FAQ";
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: color,
-      elevation: 3.0,
-      child: InkWell(
-        onTap: () {
-          _navigate(context);
-        },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ListTile(
-                title: Text(
-                  _getCategory(),
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontStyle: FontStyle.italic
-                  ),
-                ),
-                leading: _getIcon(),
-              ),
-              ListTile(
-                title: Text(
-                  title,
-                  //maxLines: 3,
-                ),
-              ),
-            ],
-          )
-      ),
     );
   }
 }
---
+++ prep/lib/widgets/appointment_prep/category_card.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'package:prep/screens/recipeList.screen.dart';
+import 'package:prep/screens/information_parser.screen.dart';
+import 'package:prep/screens/category.screen.dart';
+import 'package:prep/screens/faq_parser.screen.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String contents;
+  final String title;
+  final String type;
+  final Color color;
+  final DateTime _appointmentDateTime;
+
+  CategoryCard(this.contents, this.title, this.type, this.color, this._appointmentDateTime);
+
+  Future _navigate(dynamic context) {
+    switch (type) {
+      case "informations":
+        return Navigator.push(context, MaterialPageRoute(builder: (context) => InformationParser(contents)));
+      case "categoryList":
+        return Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryListParser(contents)));
+      case "recipeView":
+        return Navigator.push(context,
+            MaterialPageRoute(builder: (context) => RecipeListScreen()));
+      default:
+        return Navigator.push(context, MaterialPageRoute(builder: (context) => FaqParser(_appointmentDateTime)));
+    }
+  }
+
+  CircleAvatar _getIcon() {
+    if (type == "categoryList") {
+      return CircleAvatar(
+        backgroundColor: Colors.blue[400],
+        child: Icon(
+          Icons.list,
+          color: Colors.white,
+        ),
+      );
+    } else if (type == "informations") {
+      return CircleAvatar(
+        backgroundColor: Colors.deepPurple[400],
+        child: Icon(
+          Icons.info,
+          color: Colors.white,
+        ),
+      );
+    } else if (type == "faqs") {
+      return CircleAvatar(
+        backgroundColor: Colors.green[400],
+        child: Icon(
+          Icons.live_help,
+          color: Colors.white,
+        ),
+      );
+    } else {
+      return CircleAvatar(
+        backgroundColor: Colors.red[400],
+        child: Icon(
+          Icons.local_dining,
+          color: Colors.white,
+        ),
+      );
+    }
+  }
+
+  String _getCategory(){
+    switch (type) {
+      case "informations":
+        return "Info";
+      case "categoryList":
+        return "List";
+      case "recipeView":
+        return "Recipe";
+      default:
+        return "FAQ";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: color,
+      elevation: 3.0,
+      child: InkWell(
+          onTap: () {
+            _navigate(context);
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  _getCategory(),
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontStyle: FontStyle.italic
+                  ),
+                ),
+                leading: _getIcon(),
+              ),
+              ListTile(
+                title: Text(
+                  title,
+                  //maxLines: 3,
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
