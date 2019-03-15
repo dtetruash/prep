baf3f79b45e6433b73548417d9c38d3971ad62c3
David Temple <david.temple@kcl.ac.uk>
2019-03-15 12:55:13 +0000

    Add dropdown lists to ViewTests

--- prep/src/components/tests/ViewTests.vue
+++ prep/src/components/tests/ViewTests.vue
@@ -1,88 +1,81 @@
 <template>
-    <div
-            id="view-test"
-            style="background-color:white; padding: 10px 50px 10px 50px; margin-top:10px"
-        >
-            <div id="mainScreen">
-            <h4 style="font-size:3em;">
-                <b>Tests</b>
-            </h4>
+    <div id="view-test" style="background-color:white; padding: 10px 50px 10px 50px; margin-top:10px">
+        <div id="mainScreen">
+            <h4 style="font-size:3em;"><b>Tests</b></h4>
 
             <div class="row">
                 <div class="col s12">
-                <div class="card-panel light-blue">
-                    <span class="card-title white-text"><i class="small material-icons">info_outline</i>Info</span>
-                    <p class="white-text">Click on a test below to reveal editing options.
-                    </p>
-                </div>
+                    <div class="card-panel light-blue">
+                        <span class="card-title white-text"><i class="small material-icons">info_outline</i>Info</span>
+                        <p class="white-text">Click on a test below to reveal editing options.</p>
+                    </div>
                 </div>
             </div>
 
-        <ul class="collapsible popout">
-            <li v-for="test in tests" v-bind:key="test.id">
-                <div class="collapsible-header">
-                    <div id="title">{{test.title}}</div>Department: {{test.department}}</div>
-                <div class="collapsible-body">
-                    <ul class="collection">
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle green">create</i>
-                            <h5 class="titles">Test description</h5>
-                            <router-link 
-                                v-bind:to="{name: 'edit-test-description', params: {test_id: test.id}}" 
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle green">done_all</i>
-                            <h5 class="titles">Daily check-ups</h5>
-                            <router-link 
-                                to="" 
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle blue">assignment</i>
-                            <h5 class="titles">Articles</h5>
-                            <router-link 
-                                to="" 
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle blue">list</i>
-                            <h5 class="titles">Lists</h5>
-                            <router-link 
-                                to="" 
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle orange">help_outline</i>
-                            <h5 class="titles">FAQs</h5>
-                            <router-link 
-                                to="" 
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                        <li class="collection-item avatar">
-                            <i class="material-icons circle red">local_dining</i>
-                            <h5 class="titles">Recipes</h5>
-                            <router-link
-                                v-bind:to="{name: 'view-recipes', params: {test_id: test.id}}"
-                                class="btn blue secondary-content"
-                            >Show/Edit</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
+            <ul class="collapsible popout">
+                <li v-for="test in tests" v-bind:key="test.id">
+                    <div class="collapsible-header">
+                        <div id="title">{{test.title}}</div>Department: {{test.department}}</div>
+                    <div class="collapsible-body">
+                        <ul class="collection">
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle green">create</i>
+                                <h5 class="titles">Test description</h5>
+                                <router-link 
+                                    v-bind:to="{name: 'edit-test-description', params: {test_id: test.id}}" 
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle green">done_all</i>
+                                <h5 class="titles">Daily check-ups</h5>
+                                <router-link 
+                                    to=""
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle blue">assignment</i>
+                                <h5 class="titles">Articles</h5>
+                                <router-link 
+                                    v-bind:to="{name: 'view-articles', params: {test_id: test.id, title: test.title}}"  
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle blue">list</i>
+                                <h5 class="titles">Lists</h5>
+                                <router-link 
+                                    to="" 
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle orange">help_outline</i>
+                                <h5 class="titles">FAQs</h5>
+                                <router-link 
+                                    to="" 
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle red">local_dining</i>
+                                <h5 class="titles">Recipes</h5>
+                                <router-link
+                                    v-bind:to="{name: 'view-recipes', params: {test_id: test.id}}"
+                                    class="btn blue secondary-content"
+                                >Show/Edit</router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
 
-        <router-link to="/add-test" class="btn green" style="margin:20px">
-            Add Test
-        </router-link>
-
+            <router-link to="/add-test" class="btn green" style="margin:20px">
+                Add Test
+            </router-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
