b0700bf445680ac63f15af9ff35483009dd87ae3
codedario <darionunez1999@gmail.com>
2019-03-15 19:17:14 +0000

    Refactor the calendar card class

---
+++ prep/lib/widgets/dashboard/calendar_card.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:prep/screens/appointment.screen.dart';
+
+class CalendarCard extends StatelessWidget {
+  final String name;
+  final String location;
+  final DateTime dateTime;
+  final String testID;
+  final String doctorName;
+  final String testName;
+  List<Color> colors = [Colors.green[300], Colors.red[300], Colors.blue[300], Colors.orange[300]];
+  Color color;
+
+  CalendarCard(this.name, this.location, this.dateTime, this.testID, this.doctorName, this.testName) {
+    print(name.hashCode);
+    color = colors[name.hashCode % 4];
+  }
+
+  String dateFormater(DateTime datetime) {
+    const List<String> months = [
+      "January", "February", "March", "April",
+      "May", "June", "July", "August",
+      "September", "October", "November", "December"
+    ];
+
+    String day = datetime.day.toString();
+    String month = months[datetime.month - 1];
+    String year = datetime.year.toString();
+    String hour = datetime.hour.toString();
+    String minute = datetime.minute.toString();
+
+    return day + " " + month + " " + year;
+  }
+
+  String timeFormater(DateTime datetime) {
+    String hour = (datetime.hour < 10) ? "0" + datetime.hour.toString() : datetime.hour.toString();
+    String minute = (datetime.minute < 10) ? "0" + datetime.minute.toString() : datetime.minute.toString();
+
+    //String timeOfDay = (int.parse(hour) < 12) ? "am" : "pm";
+
+    return hour + " : " + minute;
+  }
+
+  Widget _informationRow(String label, String content) {
+    return Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                label,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                content,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            )
+          ],
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+      child: Card(
+        elevation: 3.0,
+        child: Stack(
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                        color: color
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  testName,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Divider(
+                                color: Colors.transparent,
+                                height: 30.0,
+                              ),
+                              _informationRow("Location: ", location),
+                              _informationRow("Staff member: ", doctorName),
+                              _informationRow("Date: ", dateFormater(dateTime)),
+                              _informationRow("Time: ", timeFormater(dateTime)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+                ListTile(
+                  leading: Icon(Icons.code),
+                  title: Text(name),
+                ),
+              ],
+            ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                    splashColor: Color.fromRGBO(255, 255, 255, 0.2),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Appointment(name, testID, 0, dateTime))
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
