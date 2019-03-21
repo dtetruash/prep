62f22319b1bde194fa3969ae3f3a35f2e709642f
Mufid Alkhaddour <k1763557@kcl.ac.uk>
2019-03-21 20:27:09 +0000

    Add mixin for lists

--- prep/src/components/tests/NewList.vue
+++ prep/src/components/tests/NewList.vue
@@ -60,59 +60,10 @@
 </template>
 
 <script>
-import db from "../firebaseInit";
+import { listsMixin } from "../../mixins/listsMixin.js";
 export default {
   name: "new-list",
-  data() {
-    return {
-      title: null,
-      prepCardID: null,
-      allMaps: [], // newly added maps
-      category: "categoryList"
-    };
-  },
-  methods: {
-    createPrepCard() {
-      db.collection("tests")
-        .doc(this.$route.params.test_id)
-        .collection("prepCards")
-        .add({
-          title: this.title,
-          contents: this.title,
-          type: this.category,
-          maps: this.allMaps
-        })
-        // reroutes to all the lists
-        .then(docRef => {
-          alert("List added!");
-          this.$router.push({
-            name: "view-prep-lists",
-            params: { test_id: this.$route.params.test_id }
-          });
-        })
-        .catch(error => console.log(err));
-    },
-    // adds a new map to the array
-    addMap() {
-      let data = new Object();
-      data.name = "";
-      data.description = "";
-      data.list = [];
-      this.allMaps.push(data);
-    },
-    // adds an item to the list of the new map
-    addToList(map) {
-      const data = "";
-      map.list.push(data);
-    },
-    // deletes an item from the list of the new map
-    deleteFromList(map, index) {
-      map.list.splice(index, 1);
-    },
-    // deletes newly added map
-    deleteMap(index) {
-      this.allMaps.splice(index, 1);
-    },
-  }
+  mixins: [listsMixin],
 };
+
 </script>
