99b306fa6b36c7fdf7f7ed1e5ee0b41b449ef811
David Temple <david.temple@kcl.ac.uk>
2019-03-21 22:01:39 +0000

    Update new and edit recipe styles

--- prep/src/components/tests/recipes/NewRecipe.vue
+++ prep/src/components/tests/recipes/NewRecipe.vue
@@ -12,12 +12,12 @@
                 <form @submit.prevent="saveRecipe" class="col s12">
                     <div class="row">
                         <div class="input-field col s12">
-                            <input type="text" v-model="title" required>
-                            <label>Title *</label>
+                            <input type="text" v-model="title" id="title" required>
+                            <label for="title">Title *</label>
                         </div>
                         <div class="input-field col s12">
-                            <input type="text" v-model="subtitle">
-                            <label>Subtitle</label>
+                            <input type="text" id="subtitle" v-model="subtitle">
+                            <label for="subtitle">Subtitle</label>
                         </div>
                     </div>
                     <div>
@@ -31,49 +31,55 @@
                     <div>
                         <div>
                             <label id="heading">Ingredients</label>
-                            <hr>
                         </div>
-                        <button @click="addIngredient" class="btn green"> add ingredient</button>
+                        <button @click="addIngredient" class="btn green" id="addBtn" type="button"> add ingredient</button>
                         <div class="row">
                             <div v-for="(ingredient, index) in ingredients" v-bind:key="index" class="input-field col s12">
-                                <input type="text" v-model="ingredients[index]" required>
-                                <button @click="deleteIngredient(index)" class="btn red" type="button">remove ingredient</button>
+                                <div class="valign-wrapper">
+                                    <input type="text" v-model="ingredients[index]" required>
+                                    <a class="btn-floating red" @click="deleteIngredient(index)" id="removeBtn"><i class="material-icons">close</i></a>
+                                </div>
                             </div>
                         </div>
                         <div>
                             <label id="heading">Instructions</label>
-                            <hr>
                         </div>
-                        <button @click="addInstruction" class="btn green"> add instruction</button>
+                        <button @click="addInstruction" class="btn green" id="addBtn" type="button"> add instruction</button>
                         <div class="row">
                             <div v-for="(instruction, index) in instructions" v-bind:key="index" class="input-field col s12">
-                                <input type="text" v-model="instructions[index]" required>
-                                <button @click="deleteInstruction(index)" class="btn red" type="button">remove instruction</button>
+                                <div class="valign-wrapper">
+                                    <input type="text" v-model="instructions[index]" required>
+                                    <a class="btn-floating red" @click="deleteInstruction(index)" id="removeBtn"><i class="material-icons">close</i></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div id="tags">
-                        <!-- chips component for recipe tags -->
-                        <div class="chips chips-placeholder"></div>
-                    </div>
-                    <div>
-                        <label id="heading">Type</label>
-                        <hr>
+                    <div class="row">
+                        <div id="tags" class="input-field col s6">
+                            <!-- chips component for recipe tags -->
+                            <div class="chips chips-placeholder"></div>
+                        </div>
                     </div>
                     <div class="row">
-                        <select class="browser-default" style="color:black" v-model="type">
+                        <div class="input-field col s6">
+                            <select  v-model="type">
+                            <option value="" disabled selected><label>Choose Recipe type</label></option>
                             <option v-for="type in recipeTypes" v-bind:key="type.index" :value="type">{{type}}</option>
-                        </select>
+                            </select>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <input type="text" v-model="note">
-                            <label>Notes</label>
+                            <textarea id="notes" class="materialize-textarea" v-model="note">
+                            </textarea>
+                            <label for="notes">Notes</label>
                         </div>
                     </div>
                     <imageUploader 
+                    id="addBtn"
                     v-on:downloadURL="imageURL=$event"
                     />
+                    <br>
                     <button type="submit" class="btn">Submit</button>
                     <router-link v-bind:to="{name: 'view-recipes', params: {test_id: this.$route.params.test_id}}" class="btn grey">Cancel</router-link>
                 </form>
@@ -138,6 +144,9 @@ export default {
         $('.chips-placeholder').chips({
             placeholder: 'Enter a tag'
         })
+        $(document).ready(function() {
+            $('select').formSelect()
+        })    
     }
 }
 </script>
@@ -148,5 +157,8 @@ export default {
 }
 #tags {
     padding-bottom: 15px
+}
+#addBtn {
+    margin: 10px;
 }
 </style>
