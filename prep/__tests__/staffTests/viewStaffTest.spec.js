35c7e61d28677bbf44ae8fc5ff19cfb0d8c7d5ff
PreslavKisyov <preslavkisyov98@gmail.com>
2019-03-22 14:24:20 +0000

    Fix an issue with jest plugins

--- prep/__tests__/staffTests/viewStaffTest.spec.js
+++ prep/__tests__/staffTests/viewStaffTest.spec.js
@@ -1,1 +1,17 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount } from "@vue/test-utils";
+// import ComponentFunctions from '../../src/mixins/usersMixin/userMixin'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import db from "../../src/components/firebaseInit";
+import firebase from "firebase";
+// @ts-ignore
+import Component from "../../src/components/staff/ViewStaff.vue";
+
+Vue.use(VueRouter);
+describe("Component", () => {
+  const wrapper = shallowMount(Component);
+  test("is a Vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+  db.app.delete();
+});
