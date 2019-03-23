eff7e35cffb60227fe018afcfb43256b7bbdc3f5
David Simon Tetruashvili <davidsimon.t@gmail.com>
2019-03-23 22:26:56 +0000

    :sparkles: Implement BackendProvider

---
+++ prep/lib/utils/backend_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:prep/utils/query.dart';
+
+///Provides an instance of and object that implements BaseBackend to pass down a widget tree
+///in order to avoid passing it down through the constructor.
+class BackendProvider extends InheritedWidget {
+  final BaseBackend backend;
+
+  BackendProvider({Key key, @required Widget child, @required this.backend})
+      : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static BaseBackend of(BuildContext context) =>
+      (context.inheritFromWidgetOfExactType(BackendProvider) as BackendProvider)
+          .backend;
+}
