8e5e24a5ece281d3489a912bcd43f6f3391c4d05
David Simon Tetruashvili <davidsimon.t@gmail.com>
2019-03-27 14:36:12 +0000

    :wheelchair: Add error message if no recipe given to recipe card

--- prep/lib/utils/constants.dart
+++ prep/lib/utils/constants.dart
@@ -5,4 +5,8 @@ class Constants {
   static const String kViewRecipeOnlineButtonText = "VIEW RECIPE ONLINE";
   static const String kMethodListHeadline = "Method";
   static const String kRecipeNotePrefix = "Note:";
+
+  static const String kErrorPrefix = "Error:";
+  static const String kErrorRecipeNotFound =
+      "$kErrorPrefix Recipe data not found.";
 }
