d20c28d36cb56e04030715afc255c8b9b9b6255f
David Simon Tetruashvili <davidsimon.t@gmail.com>
2019-03-27 14:43:43 +0000

    :sparkles: Add default data values to recipe card

--- prep/lib/utils/constants.dart
+++ prep/lib/utils/constants.dart
@@ -1,10 +1,13 @@
 class Constants {
   static const bool kIsDebug = true;
 
+  //Recipe Constants
   static const String kIngredientListHeadline = "Show Ingredients";
   static const String kViewRecipeOnlineButtonText = "VIEW RECIPE ONLINE";
   static const String kMethodListHeadline = "Method";
   static const String kRecipeNotePrefix = "Note:";
+  static const String kDefaultRecipeType = "default";
+  static const String kDefaultRecipeTitle = "Recipe";
 
   static const String kErrorPrefix = "Error:";
   static const String kErrorRecipeNotFound =
